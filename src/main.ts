e44d0033de226e2ebb82ebcec6e9976b6fe93e36
MaxinC0de <pro.maximebreton@gmail.com>
2025-10-17 20:01:11 +0200

    -

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  app.enableCors({
+    origin: [
+      "http://localhost:5173",
+      "https://pantar-f.vercel.app",
+      "https://maxime-rose.vercel.app",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  })
 
   await app.listen(process.env.PORT || 3000)
 }
